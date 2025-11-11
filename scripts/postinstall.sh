04a92b28232eebacffe9d29b11c1e3d4f08d02d5
David Brooks <d.brooks@auckland.ac.nz>
2025-11-12 10:54:03 +1300

    Add BG-RDF as a git submodule.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,6 @@
 #!bin/sh
 
 cp ./node_modules/oxigraph/web* ./src/renderer/public/oxigraph
+
+cp ./BG-RDF/schema/ontology.ttl ./src/renderer/src/assets/bg-rdf
+cp ./BG-RDF/templates/*.ttl ./src/renderer/src/assets/bg-rdf/templates
