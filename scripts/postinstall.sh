6d50b3cae48107804bef8606eb60f0f0060e2874
David Brooks <d.brooks@auckland.ac.nz>
2025-11-13 10:10:10 +1300

    Keep a copy of BG-RDF in `/public` until we can get asset loading properly configured.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,3 +4,6 @@ cp ./node_modules/oxigraph/web* ./src/renderer/public/oxigraph
 
 cp ./BG-RDF/schema/ontology.ttl ./src/renderer/src/assets/bg-rdf
 cp ./BG-RDF/templates/*.ttl ./src/renderer/src/assets/bg-rdf/templates
+
+cp ./BG-RDF/schema/ontology.ttl ./src/renderer/public/bg-rdf
+cp ./BG-RDF/templates/*.ttl ./src/renderer/public/bg-rdf/templates
