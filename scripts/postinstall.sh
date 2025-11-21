4f2cf2dd52bd61a3d268003184a1130b9cc91751
David Brooks <d.brooks@auckland.ac.nz>
2025-11-21 18:00:11 +1300

    Copy Pyodide's files and our Python wheels into `/public/pyodide`.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,6 +4,11 @@ mkdir -m 755 -p ./src/renderer/public/oxigraph
 cp ./node_modules/oxigraph/web* ./src/renderer/public/oxigraph
 
 mkdir -m 755 -p ./src/renderer/public/bg-rdf
-mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
 cp ./BG-RDF/schema/ontology.ttl ./src/renderer/public/bg-rdf
+mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
 cp ./BG-RDF/templates/*.ttl ./src/renderer/public/bg-rdf/templates
+
+mkdir -m 755 -p ./src/renderer/public/pyodide
+cp ./node_modules/pyodide/* ./src/renderer/pyodide/wheels
+mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
+cp ./python/wheels/*.whl ./src/renderer/public/pyodide/wheels
