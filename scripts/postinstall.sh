97114beecb23f4623ad9d4b4d9a4652c6c379200
David Brooks <d.brooks@auckland.ac.nz>
2025-12-20 14:41:44 +1300

    Load and configure the Python environment as part of setting up our editor app, instead of from `index.html`.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,12 +3,14 @@
 mkdir -m 755 -p ./src/renderer/public/oxigraph
 cp -p ./node_modules/oxigraph/web* ./src/renderer/public/oxigraph
 
+# Copy pyodide into public
+mkdir -m 755 -p ./src/renderer/public/pyodide
+cp -p ./node_modules/pyodide/* ./src/renderer/public/pyodide
+
+# Copy Python packages into public
+mkdir -m 755 -p ./src/renderer/public/python/wheels
+cp -p ./python/wheels/*.whl ./src/renderer/public/python/wheels
 mkdir -m 755 -p ./src/renderer/public/bg-rdf
 cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/public/bg-rdf
 mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
 cp -p ./BG-RDF/templates/*.ttl ./src/renderer/public/bg-rdf/templates
-
-mkdir -m 755 -p ./src/renderer/public/pyodide
-cp -p ./node_modules/pyodide/* ./src/renderer/public/pyodide
-mkdir -m 755 -p ./src/renderer/public/pyodide/wheels
-cp -p ./python/wheels/*.whl ./src/renderer/public/pyodide/wheels
