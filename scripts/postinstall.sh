f42c602dd5546b46991e1e45db05e5432a356d48
David Brooks <d.brooks@auckland.ac.nz>
2025-12-24 12:43:31 +1300

    Import BG-RDF into `bondgraph` plugin from `src/assets`.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -11,6 +11,14 @@ cp -p ./node_modules/pyodide/* ./src/renderer/public/pyodide
 # Copy Python packages into public
 mkdir -m 755 -p ./src/renderer/public/python/wheels
 cp -p ./python/wheels/*.whl ./src/renderer/public/python/wheels
+
+# Copy BG-RDF into assets for importing into `bondgraph` plugin
+mkdir -m 755 -p ./src/renderer/src/assets/bg-rdf
+cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/src/assets/bg-rdf
+mkdir -m 755 -p ./src/renderer/src/assets/bg-rdf/templates
+cp -p ./BG-RDF/templates/*.ttl ./src/renderer/src/assets/bg-rdf/templates
+
+# Copy BG-RDF into public for loading by `bg2cellml` Python code
 mkdir -m 755 -p ./src/renderer/public/bg-rdf
 cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/public/bg-rdf
 mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
