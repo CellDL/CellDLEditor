c61e536f5b2c4388210cfb2a878f927a5f9e3899
David Brooks <d.brooks@auckland.ac.nz>
2026-01-15 11:11:00 +1300

    Make sure the directories created during `postinstall` have sensible permissions.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,25 +1,28 @@
 #!bin/sh
 
+# We want directories to have a mode of 755
+umask 022
+
 # Copy oxigraph into assets
-mkdir -m 755 -p ./src/renderer/src/assets/oxigraph
+mkdir -p ./src/renderer/src/assets/oxigraph
 cp -p ./node_modules/oxigraph/web* ./src/renderer/src/assets/oxigraph
 
 # Copy pyodide into public
-mkdir -m 755 -p ./src/renderer/public/pyodide
+mkdir -p ./src/renderer/public/pyodide
 cp -p ./node_modules/pyodide/* ./src/renderer/public/pyodide
 
 # Copy Python packages into public
-mkdir -m 755 -p ./src/renderer/public/python/wheels
+mkdir -p ./src/renderer/public/python/wheels
 cp -p ./python/wheels/*.whl ./src/renderer/public/python/wheels
 
 # Copy BG-RDF into assets for importing into `bondgraph` plugin
-mkdir -m 755 -p ./src/renderer/src/assets/bg-rdf
+mkdir -p ./src/renderer/src/assets/bg-rdf
 cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/src/assets/bg-rdf
-mkdir -m 755 -p ./src/renderer/src/assets/bg-rdf/templates
+mkdir -p ./src/renderer/src/assets/bg-rdf/templates
 cp -p ./BG-RDF/templates/*.ttl ./src/renderer/src/assets/bg-rdf/templates
 
 # Copy BG-RDF into public for loading by `bg2cellml` Python code
-mkdir -m 755 -p ./src/renderer/public/bg-rdf
+mkdir -p ./src/renderer/public/bg-rdf
 cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/public/bg-rdf
-mkdir -m 755 -p ./src/renderer/public/bg-rdf/templates
+mkdir -p ./src/renderer/public/bg-rdf/templates
 cp -p ./BG-RDF/templates/*.ttl ./src/renderer/public/bg-rdf/templates
