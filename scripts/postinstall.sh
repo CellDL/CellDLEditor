de8c6d08a0a0a5df3edb3f5517a00b8c493a442f
David Brooks <dbrnz@users.noreply.github.com>
2026-02-20 15:23:11 +1300

    restructure: interface to `oxigraph` RDF package is now via `@celldl/editor-rdf`.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,7 +8,3 @@ mkdir -p ./src/renderer/src/assets/bg-rdf
 cp -p ./BG-RDF/schema/ontology.ttl ./src/renderer/src/assets/bg-rdf
 mkdir -p ./src/renderer/src/assets/bg-rdf/templates
 cp -p ./BG-RDF/templates/*.ttl ./src/renderer/src/assets/bg-rdf/templates
-
-# Copy oxigraph into assets
-mkdir -p ./src/renderer/src/assets/oxigraph
-cp -p ./node_modules/oxigraph/web* ./src/renderer/src/assets/oxigraph
